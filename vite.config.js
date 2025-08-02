01199bffa93b50f6b5adebf4d95b5ab07800cf2f
raga73 <a.nivas@yandex.ru>
2025-08-02 15:53:07 +0400

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
-import { resolve } from 'path'
+import path, { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default {
   root: resolve(__dirname, 'src'),
